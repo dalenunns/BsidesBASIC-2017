38cb01de47e96161a41cdb467654efc4e15297cd
Dale Nunns <dale@stuff.za.net>
2017-11-20 22:46:10 +0200

    Kinda working WebSockets

--- BsidesBASIC/BsidesBASIC.ino
+++ BsidesBASIC/BsidesBASIC.ino
@@ -1128,6 +1128,9 @@ void parse_led() {
   else
     leds[ledNo] = CRGB::Black;
   FastLED.show();
+  //TODO:Investigate why this crashes
+  //Removed as it causes a crash
+  //ws.printfAll("led%d|%d,%d,%d",ledNo,r,g,b);
 }
 
 void print(String s)
@@ -1310,5 +1313,39 @@ void onUpload(AsyncWebServerRequest *request, String filename, size_t index, uin
 }
 
 void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-  //Handle WebSocket event
+  if(type == WS_EVT_CONNECT){
+    //client connected
+    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+    client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  } else if(type == WS_EVT_DISCONNECT){
+    //client disconnected
+    Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+  } else if(type == WS_EVT_ERROR){
+    //error was received from the other end
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+  } else if(type == WS_EVT_PONG){
+    //pong message was received (in response to a ping request maybe)
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+  } else if(type == WS_EVT_DATA){
+    //data packet
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    if(info->final && info->index == 0 && info->len == len){
+      //the whole message is in a single frame and we got all of it's data
+      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+      if(info->opcode == WS_TEXT){
+        data[len] = 0;
+        Serial.printf("%s\n", (char*)data);
+      } else {
+        for(size_t i=0; i < info->len; i++){
+          Serial.printf("%02x ", data[i]);
+        }
+        Serial.printf("\n");
+      }
+      if(info->opcode == WS_TEXT)
+        client->text("I got your text message");
+      else
+        client->binary("I got your binary message");
+    } 
+  }
 }
